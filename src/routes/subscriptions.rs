4452a210b3304c79bee070c24a76c339703ba13b
PrussianPhoenix <48622357+PrussianPhoenix@users.noreply.github.com>
2023-03-08 23:26:42 +1100

    Chapter 7, Part 9
    * batch transactions together to reduce data isolation and endstates

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -1,5 +1,6 @@
 use actix_web::{web, HttpResponse};
 use sqlx::PgPool;
+use sqlx::{Postgres, Transaction};
 use uuid::Uuid;
 use chrono::Utc;
 use tracing::Instrument;
@@ -65,13 +66,21 @@ pub async fn subscribe(
         Err(_) => return HttpResponse::BadRequest().finish(),
     };
 
-    let subscriber_id = match insert_subscriber(&pool, &new_subscriber).await {
+    let mut transaction = match pool.begin().await {
+        Ok(transaction) => transaction,
+        Err(_) => return HttpResponse::InternalServerError().finish(),
+    };
+
+    let subscriber_id = match insert_subscriber(&mut transaction, &new_subscriber).await {
         Ok(subscriber_id) => subscriber_id,
         Err(_) => return HttpResponse::InternalServerError().finish(),
     };
 
     let subscription_token = generate_subscription_token();
-    if store_token(&pool, subscriber_id ,&subscription_token).await.is_err() {
+    if store_token(&mut transaction, subscriber_id ,&subscription_token).await.is_err() {
+        return HttpResponse::InternalServerError().finish();
+    }
+    if transaction.commit().await.is_err() {
         return HttpResponse::InternalServerError().finish();
     }
 
@@ -155,10 +164,10 @@ pub fn is_valid_name(s: &str) -> bool {
 //Take care of database logic
 #[tracing::instrument(
     name = "Saving new subscriber details in the database",
-    skip(new_subscriber, pool)
+    skip(new_subscriber, transaction)
 )]
 pub async fn insert_subscriber(
-    pool: &PgPool,
+    transaction: &mut Transaction<'_, Postgres>,
     new_subscriber: &NewSubscriber,
 ) -> Result<Uuid, sqlx::Error> {
     let subscriber_id = Uuid::new_v4();
@@ -174,7 +183,7 @@ pub async fn insert_subscriber(
         new_subscriber.name.as_ref(),
         Utc::now()
         )
-        .execute(pool)
+        .execute(transaction)
         .await
         .map_err(|e| {
             tracing::error!("Failed to execute query: {:?}", e);
@@ -199,9 +208,9 @@ fn generate_subscription_token() -> String {
 //Take care of database logic
 #[tracing::instrument(
 name = "Store subscription token in the database",
-skip(subscription_token, pool)
+skip(subscription_token, transaction)
 )]
-pub async fn store_token( pool: &PgPool,
+pub async fn store_token( transaction: &mut Transaction<'_, Postgres>,
                                 subscriber_id: Uuid,
                                 subscription_token: &str,
 ) -> Result<(), sqlx::Error>{
@@ -213,7 +222,7 @@ pub async fn store_token( pool: &PgPool,
         subscription_token,
         subscriber_id,
         )
-        .execute(pool)
+        .execute(transaction)
         .await
         .map_err(|e| {
             tracing::error!("Failed to execute query: {:?}", e);
